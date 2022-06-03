cb7f7af12828281aa247aae263516f963ee08f3c
Rafael <fael.vargas@hotmail.com>
2022-06-03 16:10:36 -0300

    Adição do React Router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,30 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import theme from './theme/main/theme';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 import Login from './screens/login/login';
+import { LOGIN_URL } from './screens/login/login.type';
+
+import MovieList from './screens/movies-list/movies-list';
+import { MOVIES_LIST_URL } from './screens/movies-list/movies-list.type';
+
+import { GlobalStyles } from './theme/main/global-styles';
 
 function App() {
   return (
-    <ThemeProvider theme={theme}>
-      <Login />
-    </ThemeProvider>
-  );
+    <>
+      <GlobalStyles />
+      <ThemeProvider theme={theme}>
+        <BrowserRouter>
+          <Routes>
+            <Route element={<Login />} path={LOGIN_URL} />
+            <Route element={<MovieList />} path={MOVIES_LIST_URL} />
+          </Routes>
+        </BrowserRouter>
+      </ThemeProvider>
+    </>
+  )
 }
 
 export default App;
