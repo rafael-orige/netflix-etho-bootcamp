5c0c0e02b574bb6f3a326fca178f736b0641493f
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-03 22:22:53 -0300

    Tipagem das props

--- src/components/Button/button.tsx
+++ src/components/Button/button.tsx
@@ -1,5 +1,10 @@
 import { Button as ButtonStyled } from "./button.styled";
 
-export default function Button(props: any) {
-    return <ButtonStyled {...props} />
+type Props = {
+    onClick: () => void,
+    children: string
+}
+
+export default function Button({ onClick, children }: Props) {
+    return <ButtonStyled onClick={onClick} children={children} />
 }
--- src/components/FormError/formError.tsx
+++ src/components/FormError/formError.tsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { Error } from "./formError.styled";
 
-export default function FormError(props: any) {
-    const { message, ...restProps } = props;
-    return <Error {...restProps}>{message}</Error>
+export default function FormError({ message }: { message: string }) {
+    const capitalizeMessage = message.charAt(0).toUpperCase() + message.slice(1);
+    return <Error>{capitalizeMessage + "."}</Error>
 }
