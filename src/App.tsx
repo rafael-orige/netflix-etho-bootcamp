b90c56b8fc1842be0de7a7a99871c51b07c71d9e
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-22 17:36:04 -0300

    Typescript path mapping

--- src/App.tsx
+++ src/App.tsx
@@ -2,14 +2,14 @@ import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import store from './redux/store/store';
-import theme from './theme/main/theme';
+import store from '@store/store/store';
+import theme from '@themes/main/theme';
 
-import Login from './screens/login/login';
-import { LOGIN_URL } from './screens/login/login.type';
+import Login from '@screens/login/login';
+import { LOGIN_URL } from '@screens/login/login.type';
 
-import MovieList from './screens/movies-list/movies-list';
-import { MOVIES_LIST_URL } from './screens/movies-list/movies-list.type';
+import MovieList from '@screens/movies-list/movies-list';
+import { MOVIES_LIST_URL } from '@screens/movies-list/movies-list.type';
 
 import { GlobalStyles } from './theme/main/global-styles';
 
--- src/test/redux-connected-component.tsx
+++ src/test/redux-connected-component.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import { ThemeProvider } from 'styled-components';
 import { Provider } from 'react-redux';
+import store from '@store/store/store';
 import theme from '../theme/main/theme';
-import store from '../redux/store/store';
 
 const component = (componentParameter: JSX.Element) => <Provider store={store}><ThemeProvider theme={theme}>{componentParameter}</ThemeProvider></Provider>;
 
