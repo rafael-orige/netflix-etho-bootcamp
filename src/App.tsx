8ba37e933af6eae9fc13eb3b9bec8fe17733fd91
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-23 17:25:34 -0300

    MovieList screen as private route

--- src/App.tsx
+++ src/App.tsx
@@ -5,13 +5,15 @@ import { Provider } from 'react-redux';
 import store from '@store/store/store';
 import theme from '@themes/main/theme';
 
+import { Guard } from '@components';
+
 import Login from '@screens/login/login';
 import { LOGIN_URL } from '@screens/login/login.type';
 
 import MovieList from '@screens/movies-list/movies-list';
 import { MOVIES_LIST_URL } from '@screens/movies-list/movies-list.type';
 
-import { GlobalStyles } from './theme/main/global-styles';
+import { GlobalStyles } from '@themes/main/global-styles';
 
 function App() {
   return (
@@ -21,7 +23,7 @@ function App() {
         <BrowserRouter>
           <Routes>
             <Route element={<Login />} path={LOGIN_URL} />
-            <Route element={<MovieList />} path={MOVIES_LIST_URL} />
+            <Route element={<Guard><MovieList /></Guard>} path={MOVIES_LIST_URL} />
           </Routes>
         </BrowserRouter>
       </ThemeProvider>
