c6744af21aa5329f72671ec06a1ac8600c3cdc73
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-24 17:12:19 -0300

    Call setShows from movies-list screen

--- src/screens/movies-list/movies-list.tsx
+++ src/screens/movies-list/movies-list.tsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { useAppDispatch as useDispatch } from '@store/hooks/hooks';
+import { setShows } from '@store/shows/shows.slice';
 
 export default function MovieList() {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(setShows());
+  }, []);
+
   return (
     <div>Hello, world!</div>
   );
