6314a5677326c98aceeb186fdfdaafe4f9ce71b3
Rafael Vargas <fael.vargas@hotmail.com>
2022-06-24 17:10:19 -0300

    Introducing shows into saga

--- src/redux/store/store.ts
+++ src/redux/store/store.ts
@@ -1,17 +1,28 @@
 import { configureStore } from '@reduxjs/toolkit';
-import userSaga from '@store/user/user.saga';
+import { userSaga } from '@store/user/user.saga';
+import { showsSaga } from '@store/shows/shows.saga';
 import createSagaMiddleware from 'redux-saga';
-import userSlice from '../user/user.slice';
+import userSlice from '@store/user/user.slice';
+import showsSlice from '@store/shows/shows.slice';
+import { all } from 'redux-saga/effects';
 
 const saga = createSagaMiddleware();
+
+function* storeSaga() {
+  yield all([
+    ...userSaga,
+    ...showsSaga,
+  ]);
+}
 
 const store = configureStore({
   reducer: {
     user: userSlice.reducer,
+    shows: showsSlice.reducer,
   },
   middleware: [saga],
 });
 
-saga.run(userSaga);
+saga.run(storeSaga);
 
 export default store;
